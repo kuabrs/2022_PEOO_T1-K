9efe4f7845cb417ccf1e07af6231e1885ae7c74e
kuabrs <95240062+kuabrs@users.noreply.github.com>
2022-05-16 15:00:11 -0300

    Create 2.cs

---
+++ lista1/2.cs
@@ -0,0 +1,10 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Digite sua matricula:");
+     string s = Console.ReadLine();
+    Console.WriteLine(s);  
+  }
+}

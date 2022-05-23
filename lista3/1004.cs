9f5fc3f0c33e4fb2c0e6512ac3088b941b8b2752
kuabrs <95240062+kuabrs@users.noreply.github.com>
2022-05-23 15:16:17 -0300

    Create 1004.cs

---
+++ lista3/1004.cs
@@ -0,0 +1,11 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    int A = int.Parse(Console.ReadLine());
+    int B = int.Parse(Console.ReadLine());
+    int C = A*B;
+    Console.WriteLine("PROD = "+ C);
+  }
+}

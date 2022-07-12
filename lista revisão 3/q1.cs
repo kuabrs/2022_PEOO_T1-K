f24c1ec58d9fccdba5cbf74142880c4500905336
kuabrs <95240062+kuabrs@users.noreply.github.com>
2022-07-11 22:43:48 -0300

    Create q1.cs

---
+++ lista revisão 3/q1.cs
@@ -0,0 +1,13 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    int x = 1;
+    while(x < 11){
+    Console.Write(x + " ");
+      x++;
+    }
+  }
+}
+//V

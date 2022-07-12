07b8b104002c072f8701cebdd6bd586f6493b207
kuabrs <95240062+kuabrs@users.noreply.github.com>
2022-07-11 22:45:53 -0300

    Create q02.cs

---
+++ lista revisão 3/q02.cs
@@ -0,0 +1,13 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    int x = 11;
+    while(x > 1){
+      x--;
+    Console.Write(x + " ");
+    }
+  }
+}
+//V

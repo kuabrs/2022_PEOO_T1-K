930d89a6e9e5875bf189b39344cccd2e3071b103
kuabrs <95240062+kuabrs@users.noreply.github.com>
2022-07-11 22:49:07 -0300

    Create q05.cs

---
+++ lista revisão 3/q05.cs
@@ -0,0 +1,15 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    int x = 1;
+    int y = 1;
+    while(x <=46){
+    Console.Write(x + " ");
+      x +=y;
+      y++;
+    }
+  }
+}
+//V
